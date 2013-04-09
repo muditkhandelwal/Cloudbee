6224a351451f7a6750b4b63edc5cf52239906971
mudit <mudit.khandelwal@metacube.com>
2013-04-09 17:38:28 +0530

    aa

---
+++ src/test/java/Utils.java
@@ -0,0 +1,12 @@
+
+public class Utils {
+
+    public static String readPropertyOrEnv(String key, String defaultValue) {
+        String v = System.getProperty(key);
+        if (v == null)
+            v = System.getenv(key);
+        if (v == null)
+            v = defaultValue;
+        return v;
+    }
+}
