92a5f7f28e62809f6d05ad9942b79794f04528d9
mudit <mudit.khandelwal@metacube.com>
2013-04-09 17:54:52 +0530

    test3

--- src/test/java/SauceOnDemandTest.java
+++ src/test/java/SauceOnDemandTest.java
@@ -38,22 +38,7 @@ public class SauceOnDemandTest {
         String sessionId = ((RemoteWebDriver) webDriver).getSessionId().toString();
         System.out.println("SauceOnDemandSessionID=" + sessionId);
         webDriver.get("http://qa.ringdna.com/login");
-        assertTrue(selenium.isElementPresent("//a[@class='button blue']"));
-		//selenium.click();
-		element = driver.findElement(By.xpath("//a[@class='button blue']"));
-		element.click();
-		selenium.waitForPageToLoad("50000");
-		assertTrue(selenium.isElementPresent("//input[@id='username']"));
-		assertTrue(selenium.isElementPresent("//input[@id='password']"));
-		selenium.waitForPageToLoad("50000");
-		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("demoqa01@ringdna.com");
-		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("ebmdna0198");
-		//selenium.type("//input[@id='username']", "demoqa01@ringdna.com");
-		//selenium.type("//input[@id='password']", "ebmdna0198");
-		(driver.findElement(By.xpath("//input[@id='Login']"))).click();
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='main-nav']/ul/li[1]/a[text()='Home']")));
-		//wait.until(visibilityOfElementLocated(By.id("some_id")));
-
+        
     }
 
 }
