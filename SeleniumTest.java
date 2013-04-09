6944172b5ca2d67b2f2be0aac798894733a58439
muditkhandelwal <mudit.khandelwal@metacube.com>
2013-04-09 16:16:09 +0530

    Create SeleniumTest.java

---
+++ SeleniumTest.java
@@ -0,0 +1,96 @@
+import com.saucelabs.common.SauceOnDemandAuthentication;
+import com.saucelabs.common.SauceOnDemandSessionIdProvider;
+import com.saucelabs.junit.SauceOnDemandTestWatcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
+import org.openqa.selenium.By;
+
+import java.net.URL;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Simple {@link RemoteWebDriver} test that demonstrates how to run your Selenium tests with <a href="http://saucelabs.com/ondemand">Sauce OnDemand</a>.
+ *
+ * This test also includes the <a href="">Sauce JUnit</a> helper classes, which will use the Sauce REST API to mark the Sauce Job as passed/failed.
+ *
+ * In order to use the {@link SauceOnDemandTestWatcher}, the test must implement the {@link SauceOnDemandSessionIdProvider} interface.
+ *
+ * @author Ross Rowe
+ */
+public class SeleniumTest implements SauceOnDemandSessionIdProvider {
+
+    /**
+     * Constructs a {@link SauceOnDemandAuthentication} instance using the supplied user name/access key.  To use the authentication
+     * supplied by environment variables or from an external file, use the no-arg {@link SauceOnDemandAuthentication} constructor.
+     */
+    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication("ashishsharma", "3abde8d0-d65c-4b96-992a-2f7d0881c802");
+
+    /**
+     * JUnit Rule which will mark the Sauce Job as passed/failed when the test succeeds or fails.
+     */
+    public @Rule
+    SauceOnDemandTestWatcher resultReportingTestWatcher = new SauceOnDemandTestWatcher(this, authentication);
+
+    /**
+     * JUnit Rule which will record the test name of the current test.  This is referenced when creating the {@link DesiredCapabilities},
+     * so that the Sauce Job is created with the test name.
+     */
+    public @Rule TestName testName= new TestName();
+
+    private WebDriver driver;
+
+    private String sessionId;
+
+    @Before
+    public void setUp() throws Exception {
+
+        DesiredCapabilities capabillities = DesiredCapabilities.firefox();
+        capabillities.setCapability("version", "19");
+        capabillities.setCapability("platform", Platform.XP);
+        capabillities.setCapability("name",  testName.getMethodName());
+        this.driver = new RemoteWebDriver(
+                new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"),
+                capabillities);
+        this.sessionId = ((RemoteWebDriver)driver).getSessionId().toString();
+    }
+
+    @Override
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    @Test
+    public void SeleniumTest() throws Exception {
+        driver.get("http://qa.ringdna.com/");
+        driver.findElement(By.xpath("html/body/div[3]/div/div[2]/a")).click();
+        driver.findElement(By.xpath("//*[@id='username']")).sendKeys("demoqa01@ringdna.com");
+        driver.findElement(By.xpath("//*[@id='password']")).sendKeys("ebmdna0198");
+        driver.findElement(By.xpath("//*[@id='Login']")).click();
+        driver.findElement(By.xpath("//*[@id='track']")).click();
+        driver.findElement(By.xpath("//*[@id='main-nav']/ul/li[4]/ul/li[4]/a")).click();
+  	String title = driver.findElement(By.xpath("//*[@id='header-title']")).getText();
+		if (title.equals("URL Call Tracking"))
+		{
+			System.out.println("URL Call Tracking Wizard is opened");
+			System.out.println("Test Case No.1 for Track URL is passed");
+		}else {
+			System.out.println("Test case is fail");
+		}
+		driver.findElement(By.xpath("//*[@id='userInfo']/p/a")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+}
